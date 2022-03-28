538c450b0b8f3f6086bc61c1a58d00b85b58b91c
Jev Forsberg <jevf@turbinelabs.com>
2022-03-28 12:54:25 -0600

    master/ add persistant high scores and names

--- scoreboard.py
+++ scoreboard.py
@@ -13,13 +13,24 @@ class Scoreboard(Turtle):
         super().__init__()
         self.starting_score = starting_score
         self.score = starting_score
-        self.high_score = 0
+        self.high_score = int(self.read_previous_data("score"))
+        self.high_score_player_name = self.read_previous_data("name")
         self.name = player_name
         self.color(color)
         self.penup()
         self.goto(0, (screen_height / 2 - 30))
         self.hideturtle()
         self.update_message()
+
+    def read_previous_data(self, type):
+        # note: the first line of the data.txt file is the plater name, the second is the high score.
+        type = 0 if type == "name" else 1
+        with open(file="./data.txt") as file:
+            return file.read().splitlines()[type]
+
+    def write_new_data(self):
+        with open(file="./data.txt", mode="w") as file:
+            file.write(f"{self.high_score_player_name}\n{self.high_score}")
 
     def update_score(self, new_score):
         self.score = new_score
@@ -28,10 +39,12 @@ class Scoreboard(Turtle):
     def update_message(self):
         self.clear()
         self.write(
-            f"Score: {self.score} | {self.name}'s High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+            f"{self.name}'s Score: {self.score} | {self.high_score_player_name}'s High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
+            self.high_score_player_name = self.name
             self.high_score = self.score
+            self.write_new_data()
         self.score = 0
         self.update_message()
